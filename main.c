2639a01c03f246bf39dfbc3982ad87326e9dbde4
nec0ta26z <nec0ta26z@gmail.com>
2026-07-01 17:50:41 +0900

    HoT: initial revision

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+    int heads = 0, tails = 0;
+    int result;
+
+    srand((unsigned int)time(NULL));
+
+    printf("Tossing a coin...\n");
+    for (int round = 1; round <= 3; round++) {
+        result = rand() % 2;
+        if (result == 0) {
+            printf("Round %d: Heads\n", round);
+            heads++;
+        } else {
+            printf("Round %d: Tails\n", round);
+            tails++;
+        }
+    }
+
+    printf("Heads: %d, Tails: %d\n", heads, tails);
+
+    return 0;
+}
