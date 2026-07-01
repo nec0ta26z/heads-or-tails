5e1227d75ab96625b04bdb0ff9b22ba613789080
nec0ta26z <nec0ta26z@gmail.com>
2026-07-01 18:03:05 +0900

    Final revision

--- main.c
+++ main.c
@@ -31,9 +31,9 @@ int main(void) {
     printf("Heads: %d, Tails: %d\n", heads, tails);
 
     if (heads > tails) {
-        printf("You won\n");
+        printf("%s won!\n");
     } else {
-        printf("You lost\n");
+        printf("%s lost!\n");
     }
 
     return 0;
