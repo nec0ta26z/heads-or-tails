c9f5cb2919f9aeb54aa1c82fc4af407d494ccf93
nec0ta26z <nec0ta26z@gmail.com>
2026-07-01 18:01:43 +0900

    Victory message added

--- main.c
+++ main.c
@@ -22,5 +22,11 @@ int main(void) {
 
     printf("Heads: %d, Tails: %d\n", heads, tails);
 
+    if (heads > tails) {
+        printf("You won\n");
+    } else {
+        printf("You lost\n");
+    }
+
     return 0;
 }
